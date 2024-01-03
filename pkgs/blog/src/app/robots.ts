378d0393c1a8681ea05532b0a4811aa012250e2d
Flisan <me@f4n.dev>
2024-01-03 18:06:48 +0900

    ✨ Make crawling available

--- pkgs/blog/src/app/robots.ts
+++ pkgs/blog/src/app/robots.ts
@@ -2,9 +2,16 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      disallow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        // Ref. https://netfuture.ch/2023/07/blocking-ai-crawlers-robots-txt-chatgpt/
+        userAgent: ["GPTBot", "ChatGPT-User", "Google-Extended", "FacebookBot", "OmgiliBot", "anthropic-ai", "cohere-ai"],
+        disallow: "/",
+      },
+    ]
   };
 }
